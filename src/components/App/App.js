b7f13c2a49e026447324750ef5b37d656c161256
Joseph Morse <tamed.lionheart@gmail.com>
2019-05-17 16:41:43 -0400

    feat(redux): added redux to the app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import store from '../../store'
 
 import Layout from '../Layout'
 
 const App = () => (
-  <Layout title="Connect Four" subtitle={() => <p>Under Construction</p>}>
-    <p>This is where my board will go eventually</p>
-  </Layout>
+  <Provider store={store}>
+    <Layout title="Connect Four" subtitle={() => <p>Under Construction</p>}>
+      <p>This is where my board will go eventually</p>
+    </Layout>
+  </Provider>
 )
 
 export default App
