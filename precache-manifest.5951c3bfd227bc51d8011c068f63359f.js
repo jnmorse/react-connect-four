2174fd6dc427afa3d8bf89678c6938c99b269aca
Joseph Morse <tamed.lionheart@gmail.com>
2019-05-17 17:22:40 -0400

    Updates

---
+++ precache-manifest.5951c3bfd227bc51d8011c068f63359f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d2fc93ffab874a82715ecccab451a8",
+    "url": "/jnmorse/react-connect-four/index.html"
+  },
+  {
+    "revision": "57f64a718ddf4df36e24",
+    "url": "/jnmorse/react-connect-four/static/css/main.b8ae7c4b.chunk.css"
+  },
+  {
+    "revision": "b2572fadc5e952e93388",
+    "url": "/jnmorse/react-connect-four/static/js/2.60f1ba25.chunk.js"
+  },
+  {
+    "revision": "57f64a718ddf4df36e24",
+    "url": "/jnmorse/react-connect-four/static/js/main.2cf7f703.chunk.js"
+  },
+  {
+    "revision": "371428432750e961e391",
+    "url": "/jnmorse/react-connect-four/static/js/runtime~main.ab9f40d6.js"
+  }
+]);
