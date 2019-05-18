408071641736ceffa78b107ec4a0fae77b70062d
Joseph Morse <tamed.lionheart@gmail.com>
2019-05-18 18:24:16 -0400

    chore(redux): added redux dev tools intergration

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import reducers from '../reducers'
 
-const store = createStore(reducers, applyMiddleware(thunk))
+const composeEnhancers = composeWithDevTools({})
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+  // other store enhancers if any
+)
+
+const store = createStore(reducers, enhancer)
 
 export default store
